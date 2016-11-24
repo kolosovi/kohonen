bb3c2d82e4eb0226969f017d0dada8dfb4acec8e
Kolosov Ivan <zackwag32@gmail.com>
2016-11-24 12:52:43 +0300

    Batch script

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -10,7 +10,7 @@ learning_rate_lambda_step=1000
 radius_lambda_step=1000
 
 num_procs=${num_procs:-8}
-num_procs_step=8
+num_procs_step=${num_procs_step:-1}
 num_procs_max=${num_procs_max:-256}
 
 used_slots=0
@@ -38,7 +38,7 @@ do
             used_slots=$(squeue | grep 'zackw' | wc -l)
         done
 
-        sbatch -n $num_procs -o stdout_${epochs}_epochs_${num_procs}_processes.txt -p $queue ompi -n $num_procs kohonen_learn -i test_data/experiment_data.txt -x 40 -y 40 -m 3 -e $epochs -a0 0.1 -al $learning_rate_lambda -r0 40.0 -rl $radius_lambda -o neuron_weights_${epochs}_epochs_${num_procs}_processes.txt
+        sbatch -t 15 -n $num_procs -o stdout_${epochs}_epochs_${num_procs}_processes.txt -p $queue ompi -n $num_procs kohonen_learn -i test_data/experiment_data.txt -x 40 -y 40 -m 3 -e $epochs -a0 0.1 -al $learning_rate_lambda -r0 40.0 -rl $radius_lambda -o neuron_weights_${epochs}_epochs_${num_procs}_processes.txt
 
         epochs=$[$epochs+$epochs_step]
         learning_rate_lambda=$[$learning_rate_lambda+$learning_rate_lambda_step]
